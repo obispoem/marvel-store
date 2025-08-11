94bc1980c4dfbbad5b48db3a4bf7a938dae89960
Emanuel Bispo <183718633+obispoem@users.noreply.github.com>
2025-08-10 23:30:50 -0300

    removendo keys da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
-import md5 from 'blueimp-md5';
+import md5 from 'md5';
 
 const baseURL = 'https://gateway.marvel.com/v1/public';
 
-const publicKey = '5abc687784f7c453f50e6f74b2c6accb';
-const privateKey = '3aa889fcceb6a732f038f56adc2004e04a76ff18';
+const publicKey = 'PUBLIC_KEY';
+const privateKey = 'PRIVATE_KEY';
 
 export const getComics = async (offset: number = 0) => {
   const ts = Number(new Date());
